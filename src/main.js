cfbfce4f89d3b81e8c68d3cc518ef19e1cea391d
olyaYur <141238454+olyaYur@users.noreply.github.com>
2024-01-15 22:38:55 +0200

    update js

---
+++ src/main.js
@@ -0,0 +1,85 @@
+"use strict";
+
+const form = document.querySelector('.form');
+const list = document.querySelector('.list');
+const input = document.querySelector('.input');
+/*let userInputValue = form.event.target.elements.text.value; */
+
+
+
+
+const searchParams = new URLSearchParams({
+    key: '41835868-9a86cd0490c6a90cb9e6f50a0',
+    q: 'roses',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true
+  });
+
+let url = `https://pixabay.com/api/?${searchParams}`;
+
+
+  
+console.log(url);
+
+
+form.addEventListener("click", () => {  
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Sorry, there are no images matching your search query. Please try again!");
+      }
+      return response.json();
+    })
+    .then((images) => {
+        
+      console.log(images);  
+
+      const markup = images.reduce((html, image) => {
+				return (
+                    html +
+                    `<li class= "gallery-item"> 
+                        <img src=${image.webformatURL} data-sourse=${image.largeImageURL} alt=${image.tags} />
+                        <p>likes : ${image.likes}</p>
+                        <p>views : ${image.views} </p>
+                        <p>comments: ${image.comments} </p>
+                        <p>downloads: ${image.downloads} </p>
+	        </li>`
+        );
+    }, '');
+        
+        list.insertAdjacentHTML("beforeend", markup);
+    })
+    .catch((error) => console.log(error));
+});
+
+/*test code*/
+/*const fetchUsersBtn = document.querySelector(".btn");
+const userList = document.querySelector(".user-list");
+
+fetchUsersBtn.addEventListener("click", () => {
+  fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then((users) => {
+      const markup = users.map((user) => {
+				return `<li>
+	          <p><b>Title</b>: ${user.title}</p>
+	          <p><b>url</b>: ${user.url}</p>
+	          <p><b>thumbnailUrl</b>: ${user.thumbnailUrl}</p>
+            <img src=${user.url} alt="альтернативний текст">
+	        </li>`;
+        })
+        .join("");
+        
+			userList.insertAdjacentHTML("beforeend", markup);
+    })
+    .catch((error) => console.log(error));
+});
+
+
+*/
