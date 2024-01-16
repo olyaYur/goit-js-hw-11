0bc7ac0909a3e6b49c9f38f440f3205bb17a97b9
olyaYur <141238454+olyaYur@users.noreply.github.com>
2024-01-16 18:55:53 +0000

    Deploying to gh-pages from @ olyaYur/goit-js-hw-11@0eb2d42d3ba72420683a8a354be1fc5c8146bd08 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,12 @@
-import{i as c}from"./assets/vendor-4d6948b9.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&o(l)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();let a=new URLSearchParams({key:"41835868-9a86cd0490c6a90cb9e6f50a0",q:"dog",image_type:"photo",orientation:"horizontal",safesearch:!0}),s=`https://pixabay.com/api/?${a}`;console.log(s);document.querySelector(".form");document.querySelector(".todo-list");const u=document.querySelector(".images-list");fetch(s).then(r=>{if(!r.ok)throw new Error("Sorry, there are no images matching your search query. Please try again!");return r.json()}).then(r=>{let{total:n,totalHits:i,hits:o}=r;return console.log(r),console.log(n),console.log(i),console.log(o),{total:n,totalHits:i,hits:o}=r}).then(({hits:r})=>{const n=r.reduce((i,o)=>i+`
-      <li>${o.likes}</li>
-      <li>${o.downloads}</li>
-      <li>${o.comments}</li>
-      <li>${o.views}</li>
-      <li><img src =${o.webformatURL} alt =${o.tags} /></li>
-      
-      `,"");u.innerHTML=n,console.log(r)}).catch(r=>{c.error({position:"bottomCenter",title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})});
+import{i as a,S as c}from"./assets/vendor-46aac873.js";(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();let m=new URLSearchParams({key:"41835868-9a86cd0490c6a90cb9e6f50a0",q:"dog",image_type:"photo",orientation:"horizontal",safesearch:!0}),s=`https://pixabay.com/api/?${m}`;console.log(s);document.querySelector(".form");const u=document.querySelector(".gallery");fetch(s).then(o=>{if(!o.ok)throw new Error("Sorry, there are no images matching your search query. Please try again!");return o.json()}).then(o=>{let{total:l,totalHits:n,hits:r}=o;return console.log(o),console.log(l),console.log(n),console.log(r),{total:l,totalHits:n,hits:r}=o}).then(({hits:o})=>{const l=o.reduce((n,r)=>n+`
+     <li>
+        <a class="gallery-link" href="${r.largeImageURL}">
+          <img src =${r.webformatURL} alt =${r.tags} />
+        </>  
+     </li> 
+      <li><b>Likes</b> ${r.likes}</li>
+      <li><b>Views</b> ${r.views}</li>
+      <li><b>Comments</b> ${r.comments}</li>
+      <li><b>Downloads</b> ${r.downloads}</li>
+      `,"");u.innerHTML=l,console.log(o)}).catch(o=>{a.error({position:"bottomCenter",title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})});var f=new c(".gallery a",{captionsData:"alt",captionDelay:250});f.refresh();
 //# sourceMappingURL=commonHelpers.js.map
