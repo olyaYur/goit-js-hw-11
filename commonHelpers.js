cccf455002279a838bb8d381e838eb44aa2f5425
olyaYur <141238454+olyaYur@users.noreply.github.com>
2024-01-16 19:36:01 +0000

    Deploying to gh-pages from @ olyaYur/goit-js-hw-11@1c8a8afa5ac6c405d0c779cf022a7109b30e1f40 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,12 +1,28 @@
-import{i as a,S as c}from"./assets/vendor-46aac873.js";(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();let m=new URLSearchParams({key:"41835868-9a86cd0490c6a90cb9e6f50a0",q:"dog",image_type:"photo",orientation:"horizontal",safesearch:!0}),s=`https://pixabay.com/api/?${m}`;console.log(s);document.querySelector(".form");const u=document.querySelector(".gallery");fetch(s).then(o=>{if(!o.ok)throw new Error("Sorry, there are no images matching your search query. Please try again!");return o.json()}).then(o=>{let{total:l,totalHits:n,hits:r}=o;return console.log(o),console.log(l),console.log(n),console.log(r),{total:l,totalHits:n,hits:r}=o}).then(({hits:o})=>{const l=o.reduce((n,r)=>n+`
-     <li>
-        <a class="gallery-link" href="${r.largeImageURL}">
-          <img src =${r.webformatURL} alt =${r.tags} />
-        </>  
+import{i,S as c}from"./assets/vendor-46aac873.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const o of a.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function l(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerpolicy&&(a.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?a.credentials="include":e.crossorigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function s(e){if(e.ep)return;e.ep=!0;const a=l(e);fetch(e.href,a)}})();let p=new URLSearchParams({key:"41835868-9a86cd0490c6a90cb9e6f50a0",q:"dog",image_type:"photo",orientation:"horizontal",safesearch:!0}),n=`https://pixabay.com/api/?${p}`;console.log(n);document.querySelector(".form");const g=document.querySelector(".gallery");fetch(n).then(r=>{if(!r.ok)throw new Error("Sorry, there are no images matching your search query. Please try again!");return r.json()}).then(r=>{let{total:t,totalHits:l,hits:s}=r;return console.log(r),console.log(t),console.log(l),console.log(s),{total:t,totalHits:l,hits:s}=r}).then(({hits:r})=>{const t=r.reduce((l,s)=>l+`
+     <li class="gallery-list">
+        <a class="gallery-link" href="${s.largeImageURL}">
+          <img class ="gallery-image" src =${s.webformatURL} alt =${s.tags} />
+        </>
+        <span class="gallery-wrapper">
+          <span class="gallery-tit-wrap">  
+            <span class="gallery-subtitle"><b>Likes</b></span>
+            <span class="gallery-sub-title-number">${s.likes}</span>
+          </span>
+          <span class="gallery-tit-wrap">  
+            <span class="gallery-subtitle"><b>Views</b></span>
+            <span class="gallery-sub-title-number">${s.views}</span>
+          </span>
+          <span class="gallery-tit-wrap">  
+            <span class="gallery-subtitle"><b>Comments</b></span>
+            <span class="gallery-sub-title-number">${s.comments}</span>
+          </span>
+          <span class="gallery-tit-wrap">  
+            <span class="gallery-subtitle"><b>Downloads</b></span>
+            <span class="gallery-sub-title-number">${s.downloads}</span>
+          </span>
+        </span>
      </li> 
-      <li><b>Likes</b> ${r.likes}</li>
-      <li><b>Views</b> ${r.views}</li>
-      <li><b>Comments</b> ${r.comments}</li>
-      <li><b>Downloads</b> ${r.downloads}</li>
-      `,"");u.innerHTML=l,console.log(o)}).catch(o=>{a.error({position:"bottomCenter",title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})});var f=new c(".gallery a",{captionsData:"alt",captionDelay:250});f.refresh();
+      
+      
+      `,"");g.innerHTML=t,console.log(r)}).catch(r=>{i.error({position:"bottomCenter",title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})});var u=new c(".gallery a",{captionsData:"alt",captionDelay:250});u.refresh();
 //# sourceMappingURL=commonHelpers.js.map
