cff3b97dd5ed80e8d23439c1df195b0fe1442508
olyaYur <141238454+olyaYur@users.noreply.github.com>
2024-01-16 20:08:52 +0200

    update js

--- src/main.js
+++ src/main.js
@@ -5,15 +5,10 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-
-
-
-const form = document.querySelector('.form');
-const list = document.querySelector('.list');
-
-/*var API_KEY = '41835868-9a86cd0490c6a90cb9e6f50a0';
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="*/
-
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 let searchParams = new URLSearchParams({
@@ -27,62 +22,64 @@ let searchParams = new URLSearchParams({
 let url = `https://pixabay.com/api/?${searchParams}`
 console.log(url);
 
-form.addEventListener("click", () => {  
-  fetch(url)
+
+const form = document.querySelector('.form');
+const list = document.querySelector('.todo-list');
+
+
+
+const imgList = document.querySelector('.images-list');
+    fetch(url)
+    
     .then((response) => {
-      if (!response.ok) {
-        throw new Error("Sorry, there are no images matching your search query. Please try again!");
+      if(!response.ok) {
+        throw new Error(
+         'Sorry, there are no images matching your search query. Please try again!',
+      ); 
       }
       return response.json();
     })
-    .then((images) => {
-        
-      console.log(images);  
-
-      const markup = images.reduce((html, image) => {
-				return (
-                    html +
-                    `<li class= "gallery-item"> 
-                        <img src=${image.webformatURL} data-sourse=${image.largeImageURL} alt=${image.tags} />
-                        <p>likes : ${image.likes}</p>
-                        <p>views : ${image.views} </p>
-                        <p>comments: ${image.comments} </p>
-                        <p>downloads: ${image.downloads} </p>
-	        </li>`
-        );
-    }, '');
-        
-        list.insertAdjacentHTML("beforeend", markup);
+    .then((value) => {
+      let {total, totalHits, hits} = value;
+      console.log(value);
+      console.log(total);
+      console.log(totalHits);
+      console.log(hits);
+       return {total, totalHits, hits} = value;     
     })
-    .catch((error) => console.log(error));
-});
+    .then(({hits}) => { const renderImg = hits.reduce((html, hit) => html + `
+      <li>${hit.likes}</li>
+      <li>${hit.downloads}</li>
+      <li>${hit.comments}</li>
+      <li>${hit.views}</li>
+      <li><img src =${hit.webformatURL} alt =${hit.tags} /></li>
+      
+      `, "");
+      imgList.innerHTML = renderImg;
+
+      console.log(hits);
+    })
+    .catch((error) =>{
+      iziToast.error({
+        position: 'bottomCenter',
+        title: 'Error',
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+    });
+    })
+
+
+
 
 /*
-const fetchUsersBtn = document.querySelector(".btn");
-const userList = document.querySelector(".user-list");
-
-fetchUsersBtn.addEventListener("click", () => {
-  fetch("https://jsonplaceholder.typicode.com/photos")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.json())
+    .then((todos)=>{
+      list.innerHTML = todos.reduce((html, todo) => html + `
+      <li>${todo.title}</li>
+      `, "");
+      console.log(todos);
     })
-    .then((users) => {
-      const markup = users.map((user) => {
-				return `<li>
-	          <p><b>Title</b>: ${user.title}</p>
-	          <p><b>url</b>: ${user.url}</p>
-	          <p><b>thumbnailUrl</b>: ${user.thumbnailUrl}</p>
-            <img src=${user.url} alt="альтернативний текст">
-	        </li>`;
-        })
-        .join("");
-        
-			userList.insertAdjacentHTML("beforeend", markup);
+    .catch((error) =>{
+       console.log(error)
     })
-    .catch((error) => console.log(error));
-});
-
 */
