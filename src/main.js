ff897fd45f8e4707dd7568752ccc660b6c323793
olyaYur <141238454+olyaYur@users.noreply.github.com>
2024-01-20 14:01:29 +0200

    update

--- src/main.js
+++ src/main.js
@@ -18,27 +18,14 @@ const loader = document.querySelector('.loader');
 
 formSearch.addEventListener("submit", searchImages);
 
+loader.style.display = 'none';
 function searchImages(event){
   event.preventDefault();
   let name = inputField.value;
-  
-  showLoader();
-
-  if (name === '') {
-    closeLoader();
-
-    iziToast.error({
-      title: 'Error',
-      timeout: '2000',
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-      messageColor: '#FAFAFB',
-      backgroundColor: '#EF4040',
-      position: 'topRight',
-    });
-    return;
+  if(name !== "") {
+    loader.style.display = 'block';
   }
-
+ 
   let searchParams = new URLSearchParams({
     key: '41835868-9a86cd0490c6a90cb9e6f50a0',
     q: name,
@@ -48,7 +35,7 @@ function searchImages(event){
   });
 
 let url = `https://pixabay.com/api/?${searchParams}`
-
+  console.log(url);
     fetch(url)
    .then(response => {
       loader.style.display = 'none';
@@ -58,21 +45,17 @@ let url = `https://pixabay.com/api/?${searchParams}`
 
       return response.json();
     })
-    .then(value)
-
-
-  
-
     .then((value) => {
       let {total, totalHits, hits} = value;
        return {total, totalHits, hits} = value;     
     })
+    
     .then(({hits}) => { 
     const renderImg = hits.reduce((html, hit) => html + `
      <li class="gallery-list">
         <a class="gallery-link" href="${hit.largeImageURL}">
           <img class ="gallery-image" src =${hit.webformatURL} alt =${hit.tags} />
-        </>
+        </a>
         <span class="gallery-wrapper">
           <span class="gallery-tit-wrap">  
             <span class="gallery-subtitle"><b>Likes</b></span>
@@ -113,13 +96,7 @@ let url = `https://pixabay.com/api/?${searchParams}`
         message: 'Sorry, there are no images matching your search query. Please try again!',
     });
     })
-
+    formSearch.reset();
 } 
 
-function showLoader() {
-  loader.style.display = 'block';
-}
-function closeLoader() {
-  loader.style.display = 'none';
-}
 
