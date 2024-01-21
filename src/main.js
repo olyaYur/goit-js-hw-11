ce4f3a050d18959c9218f179acc4c5038e6f020e
olyaYur <141238454+olyaYur@users.noreply.github.com>
2024-01-21 16:18:01 +0200

    update js

--- src/main.js
+++ src/main.js
@@ -29,13 +29,30 @@ formSearch.addEventListener("submit", searchImages);
 
 loader.style.display = 'none';
 
-function searchImages(event){
+function searchImages(event) {
   event.preventDefault();
+
   let name = inputField.value;
-  if(name !== "") {
-    loader.style.display = 'block';
+
+  gallery.innerHTML = '';
+
+  showLoader();
+
+  if (name === '') {
+    closeLoader();
+
+    iziToast.error({
+      title: 'Error',
+      timeout: '2000',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      messageColor: '#FAFAFB',
+      backgroundColor: '#EF4040',
+      position: 'topRight',
+    });
+    return;
   }
- 
+
   let searchParams = new URLSearchParams({
     key: '41835868-9a86cd0490c6a90cb9e6f50a0',
     q: name,
@@ -44,10 +61,11 @@ function searchImages(event){
     safesearch: true
   });
 
-let url = `https://pixabay.com/api/?${searchParams}`
+  let url = `https://pixabay.com/api/?${searchParams}`
   console.log(url);
-    fetch(url)
-   .then(response => {
+
+  fetch(url)
+    .then(response => {
       loader.style.display = 'none';
       if (!response.ok) {
         throw new Error('Request is not ok');
@@ -55,49 +73,76 @@ let url = `https://pixabay.com/api/?${searchParams}`
 
       return response.json();
     })
-    .then((value) => {
-      let {total, totalHits, hits} = value;
-       return {total, totalHits, hits} = value;     
-    })
-    
-    .then(({hits}) => { 
-    const renderImg = hits.reduce((html, hit) => html + `
-     <li class="gallery-list">
-        <a class="gallery-link" href="${hit.largeImageURL}">
-          <img class ="gallery-image" src =${hit.webformatURL} alt =${hit.tags} />
-        </a>
-        <span class="gallery-wrapper">
-          <span class="gallery-tit-wrap">  
-            <span class="gallery-subtitle"><b>Likes</b></span>
-            <span class="gallery-sub-title-number">${hit.likes}</span>
-          </span>
-          <span class="gallery-tit-wrap">  
-            <span class="gallery-subtitle"><b>Views</b></span>
-            <span class="gallery-sub-title-number">${hit.views}</span>
-          </span>
-          <span class="gallery-tit-wrap">  
-            <span class="gallery-subtitle"><b>Comments</b></span>
-            <span class="gallery-sub-title-number">${hit.comments}</span>
-          </span>
-          <span class="gallery-tit-wrap">  
-            <span class="gallery-subtitle"><b>Downloads</b></span>
-            <span class="gallery-sub-title-number">${hit.downloads}</span>
-          </span>
-        </span>
-     </li> 
-      `, "");
-      gallery.innerHTML = renderImg;
+    .then(images => {
+      if (images.hits.length === 0) {
+        closeLoader();
 
-    lightbox.refresh();
+        iziToast.error({
+          title: 'Error',
+          timeout: '2000',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: '#FAFAFB',
+          backgroundColor: '#EF4040',
+          position: 'topRight',
+        });
+        return;
+      }
+
+      gallery.innerHTML = addImages(images.hits);
+      const lightbox = new SimpleLightbox('.gallery a', 
+      {captionsData: "alt",
+       captionDelay: 250,
+       nav: true,
+       close: true,
+       enableKeyboard: true,
+       docClose: true,
+      });
+      lightbox.refresh();
     })
-    .catch((error) =>{
+    .catch(error =>
       iziToast.error({
-        position: 'bottomCenter',
         title: 'Error',
-        message: 'Sorry, there are no images matching your search query. Please try again!',
-    });
-    })
-    formSearch.reset();
-} 
+        timeout: '2000',
+        message: error,
+        messageColor: '#FAFAFB',
+        backgroundColor: '#EF4040',
+        position: 'topRight',
+      })
+    );
+}
 
+function addImages(images) {
+  return images.reduce((html, hit) => html + `
+  <li class="gallery-list">
+    <a class="gallery-link" href="${hit.largeImageURL}">
+      <img class ="gallery-image" src =${hit.webformatURL} alt =${hit.tags} />
+    </a>
+    <span class="gallery-wrapper">
+      <span class="gallery-tit-wrap">  
+        <span class="gallery-subtitle"><b>Likes</b></span>
+        <span class="gallery-sub-title-number">${hit.likes}</span>
+      </span>
+      <span class="gallery-tit-wrap">  
+        <span class="gallery-subtitle"><b>Views</b></span>
+        <span class="gallery-sub-title-number">${hit.views}</span>
+      </span>
+      <span class="gallery-tit-wrap">  
+        <span class="gallery-subtitle"><b>Comments</b></span>
+        <span class="gallery-sub-title-number">${hit.comments}</span>
+      </span>
+      <span class="gallery-tit-wrap">  
+        <span class="gallery-subtitle"><b>Downloads</b></span>
+        <span class="gallery-sub-title-number">${hit.downloads}</span>
+      </span>
+    </span>
+ </li> 
+  `, "");
+}
 
+function showLoader() {
+  loader.style.display = 'block';
+}
+function closeLoader() {
+  loader.style.display = 'none';
+}
